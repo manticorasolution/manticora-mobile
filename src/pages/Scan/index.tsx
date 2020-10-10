df5fe97e322b40417ee7e56b4adfc1d18dda7073
Lucas Yuri <lucasyuri56@hotmail.com>
2020-10-10 20:24:22 -0300

    feat: add RNCamera

--- src/pages/Scan/index.tsx
+++ src/pages/Scan/index.tsx
@@ -1,14 +1,43 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { RNCamera } from 'react-native-camera';
 
 const Register: React.FC = () => {
-
   return (
-    <View>
-        <Text>Register</Text>
+    <View style={styles.container}>
+      <Text>Register</Text>
+      <RNCamera
+        style={styles.preview}
+        type={RNCamera.Constants.Type.back}
+        flashMode={RNCamera.Constants.FlashMode.on}
+        onBarCodeRead={barcode => console.log(barcode)}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 
 export default Register;
