b94539e32d0666eef9886901b0c08848f800be29
Lucas Yuri <lucasyuri56@hotmail.com>
2020-10-10 20:14:01 -0300

    feature: add main route to routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Image } from 'react-native';
-
+import { NavigationContainer } from '@react-navigation/native';
+import Main from '../pages/Main';
 import Scan from '../pages/Scan';
 import Register from '../pages/Register';
-
 
 const App = createStackNavigator();
 
 const AppRoutes: React.FC = () => (
-  <App.Navigator
-    initialRouteName="Scan"
-  >
-    <App.Screen
-      name="Scan"
-      component={Scan}
-    />
-    <App.Screen
-      name="Register"
-      component={Register}
-    />
-  </App.Navigator>
+  <NavigationContainer>
+    <App.Navigator initialRouteName="Main">
+      <App.Screen
+        options={{
+          headerShown: false,
+        }}
+        name="Main"
+        component={Main}
+      />
+      <App.Screen name="Scan" component={Scan} />
+      <App.Screen name="Register" component={Register} />
+    </App.Navigator>
+  </NavigationContainer>
 );
 
 export default AppRoutes;
