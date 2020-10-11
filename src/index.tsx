f181613f0602a843ab55ee3825e0ee0edd2336de
Lucas Yuri <lucasyuri56@hotmail.com>
2020-10-11 15:26:58 -0300

    style: edit status bar and background

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,8 @@ import { View, StatusBar } from 'react-native';
 import Routes from './routes';
 
 const App: React.FC = () => (
-  <View style={{ backgroundColor: '#312e38', flex: 1 }}>
-    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+  <View style={{ backgroundColor: '#fff', flex: 1 }}>
+    <StatusBar barStyle="light-content" backgroundColor="#000" />
     <Routes />
   </View>
 );
